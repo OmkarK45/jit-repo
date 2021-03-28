2ab751ed3835876bdb4e6d6c64b01b10600ecb51
Omkar Kulkarni <omkarak45@gmail.com>
2021-03-28 18:15:40 +0530

    init:initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    // I have to restart dev server to see changes below..
+    <div className='bg-[#1da1f1]'>
+      <div>TEST</div>
+    </div>
+  )
+}
